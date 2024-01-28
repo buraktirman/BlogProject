b3a502fab2cded19bfc9aef42946bdbd0b9c0fb0
Burak Tırman <buraktrman@gmail.com>
2024-01-28 13:32:42 +0300

    Category add operation with Ajax in the Article Add page added.

--- Blog.Web/Areas/Admin/Controllers/CategoryController.cs
+++ Blog.Web/Areas/Admin/Controllers/CategoryController.cs
@@ -53,6 +53,26 @@ namespace Blog.Web.Areas.Admin.Controllers
             result.AddToModelState(this.ModelState);
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> AddWithAjax([FromBody] CategoryAddDto categoryAddDto)
+        {
+            var map = _mapper.Map<Category>(categoryAddDto);
+            var result = await _validator.ValidateAsync(map);
+
+            if (result.IsValid)
+            {
+                await _categoryService.CreateCategoryAsync(categoryAddDto);
+                _toast.AddSuccessToastMessage(Messages.Category.Add(categoryAddDto.Name), new ToastrOptions { Title = "İşlem Başarılı" });
+                return Json(Messages.Category.Add(categoryAddDto.Name));
+            }
+            else
+            {
+                _toast.AddErrorToastMessage(result.Errors.First().ErrorMessage, new ToastrOptions { Title = "İşlem Başarısız" });
+                return Json(result.Errors.First().ErrorMessage);
+            }
+        }
+
         [HttpGet]
         public async Task<IActionResult> Update(Guid categoryId)
         {

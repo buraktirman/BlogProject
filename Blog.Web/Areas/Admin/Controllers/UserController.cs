1692f4a2f51f96db37ba208560287a5ab82a9658
Burak Tırman <buraktrman@gmail.com>
2024-01-28 14:24:05 +0300

    Listing of users with roles added.

---
+++ Blog.Web/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Blog.Entity.DTOs.Users;
+using Blog.Entity.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IMapper _mapper;
+
+        public UserController(UserManager<AppUser> userManager, IMapper mapper)
+        {
+            _userManager = userManager;
+            _mapper = mapper;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            var map = _mapper.Map<List<UserDto>>(users);
+
+            foreach (var user in map)
+            {
+                var findUser = await _userManager.FindByIdAsync(user.Id.ToString());
+                var role = string.Join("", await _userManager.GetRolesAsync(findUser));
+
+                user.Role = role;
+            }
+            return View(map);
+        }
+    }
+}
